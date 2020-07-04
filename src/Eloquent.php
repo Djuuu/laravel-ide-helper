214257f43a546f178337f6febfadd2ed1e3551cf
Djuuu <julien@tardot.net>
2020-07-04 17:14:49 +0200

    ide-helper:eloquent refactor & improvements
    - add Query\Builder mixin to Eloquent\Builder
    - add Eloquent\Builder mixin to Relation

--- src/Eloquent.php
+++ src/Eloquent.php
@@ -17,6 +17,38 @@ use Barryvdh\Reflection\DocBlock\Tag;
 
 class Eloquent
 {
+    /** @var array[] */
+    private const FILES_TO_ANNOTATE = [
+        [
+            'class'       => 'Illuminate\Database\Eloquent\Model',
+            'originalDoc' => 'abstract class Model implements',
+            'mixins'      => [
+                '\Eloquent',
+                '\Illuminate\Database\Eloquent\Builder',
+            ],
+        ],
+        [
+            'class'       => 'Illuminate\Database\Eloquent\Builder',
+            'originalDoc' => 'class Builder',
+            'mixins'      => [
+                '\Illuminate\Database\Query\Builder',
+            ],
+        ],
+        [
+            'class'       => 'Illuminate\Database\Eloquent\Relations\Relation',
+            'originalDoc' => 'abstract class Relation',
+            'mixins'      => [
+                '\Illuminate\Database\Eloquent\Builder',
+            ],
+        ],
+    ];
+
+    /** @var Command */
+    private $command;
+
+    /** @var Filesystem */
+    private $files;
+
     /**
      * Write mixin helper to the Eloquent\Model
      * This is needed since laravel/framework v5.4.29
@@ -28,29 +60,47 @@ class Eloquent
      */
     public static function writeEloquentModelHelper(Command $command, Filesystem $files)
     {
-        $class = 'Illuminate\Database\Eloquent\Model';
+        $eloquent = new self($command, $files);
 
+        foreach (static::FILES_TO_ANNOTATE as $item) {
+            $eloquent->writeMixins($item['class'], $item['originalDoc'], $item['mixins']);
+        }
+    }
+
+    /**
+     * @param Command    $command
+     * @param Filesystem $files
+     */
+    private function __construct(Command $command, Filesystem $files)
+    {
+        $this->command = $command;
+        $this->files   = $files;
+    }
+
+    /**
+     * @param string $class
+     * @param string $originalDocDefault
+     * @param array  $expectedMixins
+     *
+     * @return void
+     */
+    private function writeMixins($class, $originalDocDefault, $expectedMixins)
+    {
         $reflection  = new \ReflectionClass($class);
         $namespace   = $reflection->getNamespaceName();
         $originalDoc = $reflection->getDocComment();
 
-        if (!$originalDoc) {
-            $command->info('Unexpected no document on ' . $class);
-        }
         $phpdoc = new DocBlock($reflection, new Context($namespace));
 
         $mixins = $phpdoc->getTagsByName('mixin');
-        $expectedMixins = [
-            '\Eloquent'                             => false,
-            '\Illuminate\Database\Eloquent\Builder' => false,
-            '\Illuminate\Database\Query\Builder'    => false,
-        ];
+
+        $expectedMixins = array_fill_keys($expectedMixins, false);
 
         foreach ($mixins as $m) {
             $mixin = $m->getContent();
 
             if (isset($expectedMixins[$mixin])) {
-                $command->info('Tag Exists: @mixin ' . $mixin . ' in ' . $class);
+                $this->command->info('Tag Exists: @mixin '.$mixin.' in '.$class);
 
                 $expectedMixins[$mixin] = true;
             }
@@ -59,7 +109,7 @@ class Eloquent
         $changed = false;
         foreach ($expectedMixins as $expectedMixin => $present) {
             if ($present === false) {
-                $phpdoc->appendTag(Tag::createInstance('@mixin ' . $expectedMixin, $phpdoc));
+                $phpdoc->appendTag(Tag::createInstance('@mixin '.$expectedMixin, $phpdoc));
 
                 $changed = true;
             }
@@ -79,31 +129,34 @@ class Eloquent
             Since there is no DocBlock, the declaration is used as a guide.
         */
         if (!$originalDoc) {
-            $originalDoc = 'abstract class Model implements';
-
-            $docComment .= "\nabstract class Model implements";
+            $originalDoc = $originalDocDefault;
+            $docComment  .= "\n".$originalDocDefault;
         }
 
         $filename = $reflection->getFileName();
-        if ($filename) {
-            $contents = $files->get($filename);
-            if ($contents) {
-                $count    = 0;
-                $contents = str_replace($originalDoc, $docComment, $contents, $count);
-                if ($count > 0) {
-                    if ($files->put($filename, $contents)) {
-                        $command->info('Wrote expected docblock to ' . $filename);
-                    } else {
-                        $command->error('File write failed to ' . $filename);
-                    }
-                } else {
-                    $command->error('Content did not change ' . $contents);
-                }
-            } else {
-                $command->error('No file contents found ' . $filename);
-            }
+
+        if (!$filename) {
+            $this->command->error('Filename not found '.$class);
+            return;
+        }
+
+        $contents = $this->files->get($filename);
+        if (!$contents) {
+            $this->command->error('No file contents found '.$filename);
+            return;
+        }
+
+        $count    = 0;
+        $contents = str_replace($originalDoc, $docComment, $contents, $count);
+        if ($count === 0) {
+            $this->command->error('Content did not change '.$contents);
+            return;
+        }
+
+        if ($this->files->put($filename, $contents)) {
+            $this->command->info('Wrote expected docblock to '.$filename);
         } else {
-            $command->error('Filename not found ' . $class);
+            $this->command->error('File write failed to '.$filename);
         }
     }
 }
